3cc03edd2079831fc1ae956991012f80a3663d57
younusimran <imranyounus@hotmail.com>
2019-11-03 12:45:27 +0500

    Quiz Upload

---
+++ Quiz1PY01510.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# marsheet programm
+
+per = int(input("Enter percentage obtained: "))
+if per >= 80 and per <=100:
+    print("A+ Grade")
+elif per >=70 and per < 80:
+    print("A Grade")
+elif per >=60 and per < 70:
+    print("B Grade")
+elif per >=50 and per < 60:
+    print("C Grade")
+elif per >=40 and per < 50:
+    print("D Grade")
+elif per >=33 and per < 40:
+    print("E Grade")
+elif per < 33:
+    print("Failed")
+else:
+    print("You have entered inappropriate percentage")
